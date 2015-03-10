bb8fc644f8ae8a6fee0ca443ae9735aa37dc42d9
Besland Romain <romain.besland@gmail.com>
2015-03-10 11:57:27 +0100

    GNU GPL licence added

--- move_workspace_command.sh
+++ move_workspace_command.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+#    Move workspace on another screen for i3
+#    Copyright (C) 2015  <romain.besland@cryptolab.net>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 while [ 1 ]
 do
     echo "Bouger ce bureau sur l'écran :\n\t1. De gauche (g)\n\t2. De droite (d)"
