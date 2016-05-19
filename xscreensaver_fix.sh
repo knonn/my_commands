78405e167dd9216f4fde7cb6818f4a6bb44042c2
BESLAND Romain <romain.besland@gmail.com>
2016-05-19 20:24:00 +0200

    Update xscreensaver_fix.sh

--- xscreensaver_fix.sh
+++ xscreensaver_fix.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-<<<<<<< HEAD
 apt-get update 
 
 apt-get install $(apt-cache depends xscreensaver | awk '$1~/Dépend/{printf $2" "}')
 apt-get install libx11-dev intltool libxt-dev libxext-dev libxpm-dev libgtk2.0-dev libxml2-dev libglade2-dev libatk1.0-0 libc6
-=======
 apt-get update
 
 apt-get install awk
 
 apt-get install $(apt-cache depends xscreensaver | awk '$1~/Dépend/{printf $2" "}')
 apt-get install libx11-dev intltool libxt-dev libxext-dev libxpm-dev libgtk2.0-dev libxml2-dev libglade2-dev
->>>>>>> 5cb0532c4c3e1ccf0f44201b5d685ad561feee09
 apt-get source xscreensaver
 
 cd xscreensaver-5.30
@@ -30,8 +27,4 @@ rm xscreensaver_5.30*
 
 echo "Xscreensaver Fixed"
 
-<<<<<<< HEAD
 exit 0
-=======
-exit 0
->>>>>>> 5cb0532c4c3e1ccf0f44201b5d685ad561feee09
